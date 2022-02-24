16bb51ab8f48f9f92cec70d00c85760caf8a569f
Michael A. McLeod <michael.mcleod.13@ucl.ac.uk>
2022-02-24 19:59:23 +0000

    Swap matrix transpose for matrix multiplication

---
+++ include/matrix_maths.h
@@ -0,0 +1,48 @@
+#ifndef TRANSPOSE_H
+#define TRANSPOSE_H
+
+#include <memory>
+
+template<typename T>
+void rowMajor_by_rowMajor(T **A, T **B, T **C, int N)
+{
+    for(int i = 0; i < N; i++)
+    {
+        for(int j = 0; j < N; j++)
+        {
+            for(int k = 0; k < N; k++)
+            {
+                C[i][j] = A[i][k] * B[k][j];
+            }
+        }
+    }
+}
+
+template<typename T>
+void rowMajor_by_colMajor(T **A, T **B, T **C, int N)
+{
+    for(int i = 0; i < N; i++)
+    {
+        for(int j = 0; j < N; j++)
+        {
+            for(int k = 0; k < N; k++)
+            {
+                C[i][j] = A[i][k] * B[j][k];
+            }
+        }
+    }
+}
+
+template<typename T>
+void trivial_transpose(T **A, T **B, int N)
+{
+    for(int i = 0; i < N; i++)
+    {
+        for(int j = 0; j < N; j++)
+        {
+            B[i][j] = A[j][i];
+        }
+    }
+}
+
+#endif
